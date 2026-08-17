0f920a94ed1569fb7a95a08f189dfe259cde5d34
Avishek Sarkar <avishekgaming7@gmail.com>
2026-08-18 01:34:43 +0530

    Time: 0 ms (100%), Space: 46.3 MB (96.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,10 +11,10 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
+        ListNode fast = head;
+        ListNode slow = head;
 
-        while(fast!=null && fast.next!=null){
+        while(fast != null && fast.next != null){
             fast=fast.next.next;
             slow=slow.next;
 
