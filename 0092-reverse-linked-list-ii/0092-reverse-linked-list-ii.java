610c8dc85b192ba1bdfcf64b0577d1779b29501e
Avishek Sarkar <avishekgaming7@gmail.com>
2026-08-19 21:29:51 +0530

    Time: 0 ms (100%), Space: 43 MB (67.57%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -13,24 +13,19 @@ class Solution {
         if(head == null || left==right){
             return head;
         }
-
         ListNode dummy= new ListNode(0);
         dummy.next=head;
-
         ListNode prev = dummy;
-
         for(int i=1; i<left; i++){
             prev=prev.next;
         }
         ListNode current=prev.next;
         for(int i=0; i<right-left; i++){
             ListNode next=current.next;
-
             current.next=next.next;
             next.next=prev.next;
             prev.next=next;
         }
-
         return dummy.next;
     }
 }
