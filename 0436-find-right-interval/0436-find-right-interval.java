e7a2f3d767916272bbbca6df4df0e6e8a7cad40e
Avishek Sarkar <avishekgaming7@gmail.com>
2026-08-25 01:13:26 +0530

    Time: 14 ms (56.95%), Space: 49.3 MB (94.85%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        int n = intervals.length;
-        int[] arr = new int[n];
-        int[] ans = new int[n];
+        int n=intervals.length;
+        int[] arr= new int[n];
+        int[] ans= new int[n];
 
-       
         for(int i=0; i<n; i++){
-            arr[i]=intervals[i][0];
+            arr[i]= intervals[i][0];
         }
-        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
-            map.put(arr[i],i);
+            map.put(arr[i], i);
+            
         }
         Arrays.sort(arr);
         for(int i=0; i<n; i++){
@@ -23,26 +24,29 @@ class Solution {
             }
         }
         return ans;
+
     }
 
-    public int BinarySearch(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        int ans = -1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+    public int BinarySearch(int[] arr, int target){
+        int start=0; 
+        int end=arr.length-1;
+        int ans=-1;
 
-            if (arr[mid] == target) {
+        while(start<=end){
+            int mid=start+ (end-start)/2;
+
+            if(arr[mid]==target){
                 return mid;
-            } else if (arr[mid] > target) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            }
+            else if(arr[mid]>target){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
-
         return ans;
     }
 }
