1ca8c20dd5809c1a17f156d0a1e921f9b95b2152
Avishek Sarkar <avishekgaming7@gmail.com>
2026-09-19 20:25:36 +0530

    Time: 30 ms (75.51%), Space: 55.4 MB (31.14%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,16 +1,17 @@
 class StockSpanner {
-    private Stack<int[]>stack;
+    private Stack<int[]> stack;
 
     public StockSpanner() {
-        stack= new Stack<>();
-
+        stack = new Stack<>();
     }
-    
+
     public int next(int price) {
-        int span=1;
-        while(!stack.isEmpty() && stack.peek()[0]<=price){
-            span+=stack.pop()[1];
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
         }
+
         stack.push(new int[]{price, span});
         return span;
     }
